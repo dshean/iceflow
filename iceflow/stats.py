7adfd47945d131299102197ba21a6bd357c40583
James Douglass <jamesdouglassusa@gmail.com>
2016-11-17 15:29:00 -0800

    Removing old functions and deprecated comments.

--- iceflow/stats.py
+++ iceflow/stats.py
@@ -33,34 +33,7 @@ print rasters
 timesteps = [_date_from_filename(r) for r in rasters]
 timesteps = numpy.array([(d - timesteps[0]).days for d in timesteps])
 
-import warnings
-warnings.filterwarnings('once', category=numpy.RankWarning)
-def myfunc(vector):
-    valid_mask = vector != 0
-    try:
-        return numpy.polyfit(timesteps[valid_mask], vector[valid_mask], 1)[0]
-    except TypeError:
-        return 0.
-
-#for raster_tuples in itertools.izip(*(pygeoprocessing.iterblocks(r) for r in rasters)):
-    # primitive workflow: use numpy.take to extract a single vector along the
-    # dimension of matrices in raster_tuples.  This is problematic, though,
-    # since we'll have to do this once for each pixel in the stack.
-    # Ideally, we'll be able to do this on whole stacks of blocks at a time.
-    # Approach: use apply_along_axis to calculate the regression this way.
-    # TODO: Ignore nodata values so they don't skew results.
-
-def mynewfunc_works(*blocks):
-    applied = numpy.apply_along_axis(myfunc, 2, numpy.dstack(blocks))
-    print applied.shape
-    return applied
-
-def mynewfunc(*blocks):
-    stacked_array = numpy.dstack(blocks)
-    applied = numpy.apply_along_axis(myfunc, 2, stacked_array)
-    return applied
-
-def mynewfunc_reshape(*blocks):
+def regression(*blocks):
     stacked_array = numpy.dstack(blocks)
     new_shape = (stacked_array.shape[0]*stacked_array.shape[1], 4)
     reshaped = numpy.swapaxes(numpy.reshape(stacked_array, new_shape), 0, 1)
@@ -72,7 +45,7 @@ def mynewfunc_reshape(*blocks):
 
 pygeoprocessing.vectorize_datasets(
     dataset_uri_list=rasters,
-    dataset_pixel_op=mynewfunc_reshape,
+    dataset_pixel_op=regression,
     dataset_out_uri='newfile.tif',
     datatype_out=gdal.GDT_Float32,
     nodata_out=0,
