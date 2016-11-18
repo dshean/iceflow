0ed71ed91fa54b0597429126a82dee15d6bf55dc
James Douglass <jamesdouglassusa@gmail.com>
2016-11-18 09:37:17 -0800

    Noting next steps, adding function to compare with make_stack.py.

--- iceflow/regression.py
+++ iceflow/regression.py
@@ -1,6 +1,10 @@
+# TODO: Dump comments about what I did.
+# TODO: Add a page on what we did to the wiki.
+# TODO: Add order, weights to polyfit
+
+
 import os
 import glob
-import sys
 import datetime
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -10,13 +14,34 @@ import numpy
 from osgeo import gdal
 import pygeoprocessing
 
-
-
 def _date_from_filename(filename):
     time = os.path.basename(filename).split('_')[0]
     return datetime.date(int(time[0:4]),
                          int(time[4:6]),
                          int(time[6:]))
+
+
+def _compare_with_make_stack(stack_trend_file, pgp_trend_file, diff_file):
+    stack_nodata = pygeoprocessing.get_nodata_from_uri(stack_trend_file)
+    pgp_nodata = pygeoprocessing.get_nodata_from_uri(pgp_trend_file)
+    def _diff(stack_trend, pgp_trend):
+        valid_mask = ((stack_trend != stack_nodata) & (pgp_trend != pgp_nodata))
+        out_array = numpy.empty_like(stack_trend)
+        out_array[:] = -9999
+        out_array[valid_mask] = stack_trend[valid_mask] - pgp_trend[valid_mask]
+        return out_array
+
+    pygeoprocessing.vectorize_datasets(
+        dataset_uri_list=[stack_trend_file, pgp_trend_file],
+        dataset_pixel_op=_diff,
+        dataset_out_uri=diff_file,
+        datatype_out=gdal.GDT_Float32,
+        nodata_out=-9999,
+        pixel_size_out=32.,
+        bounding_box_mode='intersection',
+        vectorize_op=False,
+        datasets_are_pre_aligned=False)
+
 
 
 def make_regression(worldview_folder, out_filename):
@@ -29,6 +54,8 @@ def make_regression(worldview_folder, out_filename):
     LOGGER.info('Timesteps: %s' % timesteps)
 
     def _regression(*blocks):
+        """Compute linear regression from a stack of DEM matrices.
+        """
         stacked_array = numpy.dstack(blocks)
         new_shape = (stacked_array.shape[0]*stacked_array.shape[1], len(timesteps))
         reshaped = numpy.swapaxes(numpy.reshape(stacked_array, new_shape), 0, 1)
@@ -47,6 +74,7 @@ def make_regression(worldview_folder, out_filename):
         bounding_box_mode='intersection',
         vectorize_op=False,
         datasets_are_pre_aligned=False)
+
 
 if __name__ == '__main__':
     make_regression('data', 'regression.tif')
