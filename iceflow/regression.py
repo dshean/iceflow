25b0e46316ecdca7b1cb0b4559366bf17bf704f2
James Douglass <jamesdouglassusa@gmail.com>
2016-11-18 10:14:07 -0800

    Adding docstrings for regression.py.

--- iceflow/regression.py
+++ iceflow/regression.py
@@ -15,6 +15,20 @@ from osgeo import gdal
 import pygeoprocessing
 
 def _date_from_filename(filename):
+    """Read in a datetime object from a Worldview raster filename.
+
+    Filenames are expected to have the format:
+
+        ``YYYYMMDD_HHMM_<otherstuff>.tif``
+
+    The first eight characters are parsed into an instance of
+    ``datetime.date`` representing this date.
+
+    Parameters:
+        filename (string): The string filename.
+
+    Returns:
+        An instance of ``datetime.date`` for the datestamp."""
     time = os.path.basename(filename).split('_')[0]
     return datetime.date(int(time[0:4]),
                          int(time[4:6]),
@@ -22,9 +36,43 @@ def _date_from_filename(filename):
 
 
 def _compare_with_make_stack(stack_trend_file, pgp_trend_file, diff_file):
+    """Compare trend of ``make_regression`` with trend from ``make_stack.py``.
+
+    Comparison is done as a per-pixel diff on any pixel pairs where both
+    pixels are not nodata.  If either pixel in a pixel stack is nodata, the
+    stack is ignored and nodata is returned for that pixel value.
+
+    The diff looks like this::
+
+        diff_file = stack_trend_file - pgp_trend_file
+
+    Parameters:
+        stack_trend_file (string): The path to the trend raster output of
+            ``make_stack.py`` (usually named ``stack_trend.tif``).  This
+            file must exist on disk.
+        pgp_trend_file (string): The path to the trend raster output from
+            ``make_regression()``, also in this module.  This file must
+            exist on disk.
+        diff_file (string): The path to where the difference raster should be
+            saved.
+
+    Returns:
+        ``None``"""
     stack_nodata = pygeoprocessing.get_nodata_from_uri(stack_trend_file)
     pgp_nodata = pygeoprocessing.get_nodata_from_uri(pgp_trend_file)
+
     def _diff(stack_trend, pgp_trend):
+        """Calculate a diff between two matrices, ignoring nodata.
+
+        Parameters:
+            stack_trend (numpy.ndarray): Array of values from the stack trend
+                raster.
+            pgp_trend (numpy.ndarray): Array of values from the pygeoprocessing
+                trend raster.
+
+        Returns:
+            ``numpy.ndarray`` of the difference between ``stack_trend`` and
+            ``pgp_trend``"""
         valid_mask = ((stack_trend != stack_nodata) & (pgp_trend != pgp_nodata))
         out_array = numpy.empty_like(stack_trend)
         out_array[:] = -9999
@@ -43,8 +91,27 @@ def _compare_with_make_stack(stack_trend_file, pgp_trend_file, diff_file):
         datasets_are_pre_aligned=False)
 
 
-
 def make_regression(worldview_folder, out_filename):
+    """Calculate a regression between worldview DEMs within a folder.
+
+    Note:
+        Any pixel stacks that contain 1 or more nodata values will be
+        excluded from the regression calculations.
+
+        Additionally, this function assumes that all worldview rasters
+        have a nodata value of ``0``.
+
+    Parameters:
+        worldview_folder (string): The path to a folder on disk containing
+            GeoTiffs representing elevation data.  Any files with a ``'.tif``
+            extension will be analyzed within this folder.  There is no
+            upper limit to the number of files that can be analyzed.
+        out_filename (string): The path on disk to where the regression raster
+            should be stored.  If the file already exists on disk, it will be
+            overwritten.
+
+    Returns:
+        ``None``"""
     rasters = sorted(glob.glob(worldview_folder + '/*.tif'),
                      key=lambda x: int(os.path.basename(x).split('_')[0]))
     LOGGER.info('Using rasters %s', rasters)
@@ -55,9 +122,24 @@ def make_regression(worldview_folder, out_filename):
 
     def _regression(*blocks):
         """Compute linear regression from a stack of DEM matrices.
+
+        Note:
+            Any pixel stacks that contain 1 or more values of 0 will have an
+            output value of ``0``.
+
+        Parameters:
+            blocks (list): A list of 2D ``numpy.ndarray`` instances with pixel
+                values from the stack of rasters passed to
+                ``vectorize_datasets`` call.  There is no upper limit to the
+                number of timesteps that can be calculated.
+
+        Returns:
+            ``numpy.ndarray``, in 2 dimensions.  This will contain the ``m``
+            parameter from the fitted line.
         """
         stacked_array = numpy.dstack(blocks)
-        new_shape = (stacked_array.shape[0]*stacked_array.shape[1], len(timesteps))
+        new_shape = (stacked_array.shape[0]*stacked_array.shape[1],
+                     len(timesteps))
         reshaped = numpy.swapaxes(numpy.reshape(stacked_array, new_shape), 0, 1)
         regression = numpy.polyfit(timesteps,
                                    reshaped, 1)[0]
